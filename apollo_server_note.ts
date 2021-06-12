e63b6a781cefacd58468333a96979ae74598b1a6
Jon Jaryong Lee <director9303@gmail.com>
2021-06-12 19:31:25 +0900

    Update apollo_server_note.ts

--- apollo_server_note.ts
+++ apollo_server_note.ts
@@ -3,68 +3,14 @@
 // 서버를 돌릴 것이기에 c드라이브 user 하위에 만들어주기로 한다.
 // 디렉터리 이름은 graphql_backend
 // git clone https://github.com/GNUCSAssistants/TypeScript-Init
+// cd TypeScript-Init
+// npm i
+// npm start
+// src/index.ts
+// 여기서 console.log()를 바꾸면, 자동으로 터미널에 반영될 것
 
-// npm init
-
-// package name: (jonydev) graphql_backend
-// version: (1.0.0)
-// description: test for graphql_backend
-// entry point: (index.js)
-// test command:
-// git repository:
-// keywords:
-// author: Jon Jaryong Lee
-// license: (ISC)
-
-// dist, src 폴더 만들어줌
-
-// npm install typescript
-// npm install tsc-watch --include=dev
-
-// package.json 수정
-{
-  "name": "graphql_backend",
-  "version": "1.0.0",
-  "description": "test for graphql_backend",
-  "main": "index.js",
-  "scripts": {
-    "start": "tsc-watch --onSuccess \" node dist/index.js\" ",
-    "prestart": "tsc"
-  },
-  "author": "Jon Jaryong Lee",
-  "license": "ISC",
-  "dependencies": {
-    "tsc-watch": "^4.4.0",
-    "typescript": "^4.3.2"
-  }
-}
-
-// tsconfig.json 파일 만들어줌(js로 컴파일 옵션)
-{
-  "compilerOptions": {
-    "module": "commonjs",
-    "target": "ES2015",
-    "sourceMap": true,
-    "outDir": "dist"
-  },
-  "include": ["src/**/*"],
-  "exclude": ["node_modules"]
-}
-
-// src 디렉터리 안에 index.ts 만들고, console.log("hello");
-
-// 그리고 npm start 하면 변화를 자동추적할것.
-
-// npm install apollo-server graqhql
-
-// src/schema.ts 
-type Launch {
-  id: ID!
-  site: String
-  mission: Mission
-  rocket: Rocket
-  isBooked: Boolean!
-}
+// npm install apollo-server 
+// npm install graqhql
 
 // src/index.ts
 import { ApolloServer } from "apollo-server";
@@ -72,12 +18,18 @@ import typeDefs from "./schema";
 
 const server = new ApolloServer({ typeDefs });
 
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `);
+});
 
 // src/schema.ts
 import { gql } from "apollo-server";
-const typeDefs = gql`
+
+export const typeDefs = gql`
   # Your schema will go here
 `;
-
-export typeDefs;
 

b8dbd8981415940c8fe19879f35df75d397f23cf
Jon Jaryong Lee <director9303@gmail.com>
2021-06-14 09:32:48 +0900

    Update apollo_server_note.ts

--- apollo_server_note.ts
+++ apollo_server_note.ts
@@ -1,7 +1,7 @@
 // 필요한 건 nodejs, git, ts-node(-g), nodemon(-g), typescript(-g)
 // 프로젝트 폴더 만들어줌
 // 서버를 돌릴 것이기에 ssd가 있으면 c드라이브 user 하위에 만들어주기로 한다.
-// 디렉터리 이름은 graphql_backend
+// git clone https://github.com/GNUCSAssistants/graphql-backend
 
-// npm install apollo-server graphql
-
+// npm i
+// npm start
